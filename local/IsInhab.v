e404b5efbef9380e4ac4c409f969f61c2b06f08e
jcmckeown <jesse@sockwright>
2012-06-06 20:47:26 -0400

    is_inhab is definable --- no new axioms needed!
    Signed-off-by: jcmckeown <jesse@sockwright>

---
+++ local/IsInhab.v
@@ -0,0 +1,54 @@
+Add LoadPath "~/HoTT-local/Coq".
+Add LoadPath "~/HoTT-local/local".
+
+Require Import Homotopy.
+
+Close Scope nat_scope.
+
+Definition IsEmpty ( X : Type ) : Type := X -> False.
+
+Definition is_inhab (X : Type ) := IsEmpty (IsEmpty X).
+
+Lemma is_conn_inhab { X : Type } : forall a b : is_inhab X, 
+ a == b.
+ intros.
+ apply funext.
+ intro.
+ contradict (a x).
+Defined.
+
+Lemma if_inhab_then_inhab_contr { X : Type } :
+ forall a : is_inhab X,
+  is_contr (is_inhab X).
+  intros.
+  exists a.
+  intros.
+  apply is_conn_inhab.
+Defined.
+
+Lemma is_inhab_is_prop (X : Type) : is_prop ( is_inhab X ).
+  intro.
+  intro.
+  exists ( is_conn_inhab x y ).
+  intros.
+  refine ( contr_path2 _ _ (if_inhab_then_inhab_contr x)).
+Defined.
+
+Fixpoint n_connected ( n : nat) : Type -> Type :=
+ match n with
+ | O => fun X => is_inhab X 
+ | S m => fun X => is_inhab X * forall x y : X, (n_connected m ( x == y )) end.
+
+(** The definition above proposes 0-connected for inhabited,
+1-connected for inhabited path-connected, 2-connected for path-simply-path-connected
+spaces, etc. I suppose every space is (-1)-connected? *)
+
+Definition component { X : Type } : X -> Type :=
+ fun x => { y : X & is_inhab (x == y) }.
+
+Definition components_are_subspaces { X : Type} ( x : X ) : component x -> X := 
+ fun y => pr1 y.
+
+(** There is a monad structure one can write on is_inhab --- it's basically 
+    because IsEmpty is a corepresentable endofunctor?
+ *)
